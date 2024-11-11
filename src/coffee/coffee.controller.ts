8b3b5717435eccbcd99e99b3750b78ec8ff23308
hbashar434 <h.bashar434@gmail.com>
2024-11-11 23:02:20 +0600

    create coffee route

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffee')
-export class CoffeeController {}
+export class CoffeeController {
+  @Get()
+  getCoffees() {
+    return [{ property: 'Hello from coffee array!' }];
+  }
+
+  @Get(':id')
+  getOneCoffee(@Param('id') id: string) {
+    return { id: id };
+  }
+}
