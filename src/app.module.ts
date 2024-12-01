bc9cf9be991a053a31660fc37338758331b21c1e
hbashar434 <h.bashar434@gmail.com>
2024-12-02 00:10:32 +0600

    setup typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './coffee/coffee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'coffee_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
